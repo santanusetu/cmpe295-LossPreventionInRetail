36890c45fc6f63e5e5217ff5f6b183672f46f948
Nagashruthi <nagashruthikb215@gmail.com>
2016-04-11 15:03:26 -0700

    Create getRedAlertUsers.java
    lists the users who missed the packages

---
+++ getRedAlertUsers.java
@@ -0,0 +1,65 @@
+package com.shruthi.lpasri.project1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.*;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/getRedAlertUsers")
+public class RedAlertUserIndicator {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	public static String show() throws Exception
+	{
+		Connection conn=getConnection();
+		ResultSet rs=getDetails(conn);
+		StringBuffer sb=new StringBuffer();
+		while(rs.next())
+		{
+			sb.append("Users who lost the packages: "+rs.getString(3)+"\n");	
+			sb.append("Destination address: "+rs.getString(9)+"\n");
+		}
+		return sb.toString();
+
+	}
+	public static Connection getConnection() throws Exception
+	{
+		try
+		{
+		String driver="com.mysql.jdbc.Driver";
+		String url="jdbc:mysql://localhost/newdb";
+		String username="root";
+		String password="";
+		Class.forName(driver);
+		Connection conn=DriverManager.getConnection(url,username,password);
+		return conn;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return null;
+	}
+	
+	public static ResultSet getDetails(Connection conn) throws Exception
+	{
+		return getResultSet(conn,"select * from shippingdetails");
+	}
+	
+	public static ResultSet getResultSet(Connection conn,String sql) throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Statement stmt=conn.createStatement(java.sql.ResultSet.CONCUR_READ_ONLY,java.sql.ResultSet.TYPE_FORWARD_ONLY);
+		return stmt.executeQuery(sql);
+	}
+
+
+}
